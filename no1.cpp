b3200a47bfcbb52ad9ebf4586c33f10534a51d73
laurenpatriciaa <163313993+laurenpatriciaa@users.noreply.github.com>
2024-04-25 14:57:53 +0700

    Create no1.cpp

---
+++ no1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int a, b = 1;
+    cout << "Masukkan angka: ";
+    cin >> a;
+    
+    for (int i = 1; i <= a; i++) 
+    {
+        
+        for (int j = 1; j <= i; j++) 
+    {
+        cout << b << " ";
+        b++;
+        }
+        cout << endl; 
+    }
+
+}
